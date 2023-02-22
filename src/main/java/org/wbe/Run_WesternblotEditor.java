3119f5b5cc5f6cb0e269dacf2dc221210325975a
jgoldmintz <101429213+jgoldmintz@users.noreply.github.com>
2023-02-22 17:38:02 -0500

    Adapted for Window's OS

--- src/main/java/org/wbe/Run_WesternblotEditor.java
+++ src/main/java/org/wbe/Run_WesternblotEditor.java
@@ -1,5 +1,7 @@
 package org.wbe;
 
+import org.im4java.process.ProcessStarter;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -7,26 +9,31 @@ import java.io.IOException;
 public class Run_WesternblotEditor {
 
     public static void main(String[] args){
-        File file1 = new File("./refresh_output.sh");
-        file1.setExecutable(true);
+
+        //String myPath="C:\\'Program Files'\\ImageMagick\\";
+        //ProcessStarter.setGlobalSearchPath(myPath);
+
+
+        // File file1 = new File("./refresh_output.sh");
+        //file1.setExecutable(true);
 
         //File dependency_checker = new File("../src/main/resources/bash_scripts/dependency_check.sh");
 
-        try {
+       /* try {
             ProcessBuilder pb = new ProcessBuilder("./refresh_output.sh");
             Process p = pb.start();
             p.waitFor();
             System.out.println("Script executed successfully");
 
-          /*  ProcessBuilder pb2 = new ProcessBuilder("../src/main/resources/bash_scripts/dependency_check.sh");
+            ProcessBuilder pb2 = new ProcessBuilder("../src/main/resources/bash_scripts/dependency_check.sh");
             Process p2 = pb2.start();
             p2.waitFor();
             System.out.println("Dependencies checked");
-*/
+
 
         } catch (IOException | InterruptedException ex) {
             throw new RuntimeException(ex);
-        }
+        }*/
         try{
 
             new UMGCWesternBlotEditor();
