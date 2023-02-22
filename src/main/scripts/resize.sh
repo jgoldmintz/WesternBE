ea1f3c648c93acf0d7228411f249b8ced60dbd99
jgoldmintz <101429213+jgoldmintz@users.noreply.github.com>
2023-02-22 17:50:02 -0500

    Delete resize.sh

--- src/main/scripts/resize.sh
+++
@@ -1,8 +0,0 @@
-# resize
-# define variables
-# VAR1 = file path
-# VAR2 = width
-# VAR3 = height
-
-magick "$VAR1" -resize "$VAR2"x"$VAR3" "$VAR4"
-printf "\nmagick %s -resize %sx%s %s" "$VAR1" "$VAR2" "$VAR3" "$VAR4" >> output.sh
